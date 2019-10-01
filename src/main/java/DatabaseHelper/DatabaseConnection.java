500be4c1e56be2fb5318598bee844e343108b80e
RahulRj09 <rahul18@navgurukul.org>
2019-10-01 15:48:44 +0530

    Add. connect method in class DatabaseConnection for connect to db

--- src/main/java/DatabaseHelper/DatabaseConnection.java
+++ src/main/java/DatabaseHelper/DatabaseConnection.java
@@ -1,4 +1,18 @@
 package DatabaseHelper;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private Connection connect() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/snakeAndLadderGame?useSSL=false", "root", "password");
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        return connection;
+    }
 }
