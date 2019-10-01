7c2186fedba231194040ddc52f2c0ac8de8b0622
RahulRj09 <rahul18@navgurukul.org>
2019-10-01 14:22:34 +0530

    Add. constructor in Signup class

--- src/main/java/authentication/Signup.java
+++ src/main/java/authentication/Signup.java
@@ -1,4 +1,13 @@
 package authentication;
 
 public class Signup {
+    private String name;
+    private String emailId;
+    private String password;
+
+    public Signup(String name, String emailId, String password) {
+        this.name = name;
+        this.emailId = emailId;
+        this.password = password;
+    }
 }
