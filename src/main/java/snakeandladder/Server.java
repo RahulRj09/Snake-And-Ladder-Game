336eac748b7a94af99f1fc5dd1e23308d98af403
RahulRj09 <rahul18@navgurukul.org>
2019-10-01 13:08:00 +0530

    Add. static MyHandler class for handle request

--- src/main/java/snakeandladder/Server.java
+++ src/main/java/snakeandladder/Server.java
@@ -1,5 +1,38 @@
 package snakeandladder;
 
-public class Server {
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.nio.file.Files;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            URI requestURI = exchange.getRequestURI();
+            File root = FileSystemView.getFileSystemView().getHomeDirectory();
+            String path = root + "/SnakeAndLadderGame/src/main/java/resources/pages/" + requestURI;
+            File file = new File(path);
+            exchange.sendResponseHeaders(200, file.length());
+            try (OutputStream os = exchange.getResponseBody()) {
+                Files.copy(file.toPath(), os);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
