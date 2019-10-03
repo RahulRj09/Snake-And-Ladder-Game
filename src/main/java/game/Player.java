005778c8a1b9b8ce460301e41974c0a4e72e5be0
RahulRj09 <rahul18@navgurukul.org>
2019-10-03 16:46:30 +0530

    Add. moveATokenOut method in player class

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -2,10 +2,18 @@ package game;
 
 public class Player {
     Token token;
+    int  numberOfTokenOut = 0;
     public Player(Token token) {
         this.token = token;
     }
 
     public void play(Dice dice) {
+        int numberOnDice = dice.roll();
+        if(numberOnDice == 1 && numberOfTokenOut ==0){
+            moveATokenOut();
+        }
+    }
+
+    private void moveATokenOut() {
     }
 }
