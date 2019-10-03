dc1b20147d98d7c72fa4423fb1d1120012f5416a
RahulRj09 <rahul18@navgurukul.org>
2019-10-03 15:08:22 +0530

    refactor. return statement in getUserDetails method

--- src/main/java/DatabaseHelper/ProfileDatabaseHelper.java
+++ src/main/java/DatabaseHelper/ProfileDatabaseHelper.java
@@ -1,5 +1,8 @@
 package DatabaseHelper;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,17 +11,24 @@ import java.sql.SQLException;
 public class ProfileDatabaseHelper {
     private DatabaseConnection databaseConnection = new DatabaseConnection();
 
-    public void getUserDetails(String emailId) {
-        String sql = "SELECT * FROM user WHERE emailId = '" + emailId +"'";
+    public JSONObject getUserDetails(String emailId) {
+        JSONObject userDetails = new JSONObject();
+
+        JSONArray userDetailsArray = new JSONArray();
+        String sql = "SELECT * FROM user WHERE emailId = '" + emailId + "'";
         try (Connection conn = databaseConnection.connect();
              PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
-             ResultSet resultSet = preparedStatement.executeQuery();
-             while (resultSet.next()){
-                 System.out.println(resultSet.getString("emailId"));
-             }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                JSONObject details = new JSONObject();
+                details.put("name", resultSet.getString("name"));
+                details.put("email", resultSet.getString("emailId"));
+                userDetailsArray.add(details);
+            }
+            userDetails.put("Profile", userDetailsArray);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
+        return userDetails;
     }
 }
--- src/main/java/authentication/ProfileHandler.java
+++ src/main/java/authentication/ProfileHandler.java
@@ -3,17 +3,25 @@ package authentication;
 import DatabaseHelper.ProfileDatabaseHelper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.json.simple.JSONObject;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
 
 public class ProfileHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-       String emailId = exchange.getRequestURI().getQuery();
+        String emailId = exchange.getRequestURI().getQuery();
         List<String> emailIdA = Arrays.asList(emailId.split("="));
         ProfileDatabaseHelper profileDatabaseHelper = new ProfileDatabaseHelper();
-        profileDatabaseHelper.getUserDetails( emailIdA.get(1));
+        JSONObject res = profileDatabaseHelper.getUserDetails(emailIdA.get(1));
+        String response = res.toString();
+        exchange.getResponseHeaders().set("Content-Type", "appication/json");
+        exchange.sendResponseHeaders(200, response.length());
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
