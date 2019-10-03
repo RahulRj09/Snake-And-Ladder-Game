8e980d0a2b5c6a33e1fce234fdc2b9b41acc553f
RahulRj09 <rahul18@navgurukul.org>
2019-10-03 09:42:57 +0530

    Add. Yard class

--- src/test/java/game/GameTest.java
+++ src/test/java/game/GameTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 public class GameTest {
     @Test
     public void gameShouldBeRun() {
+        Yard yard = new Yard();
         Player player = new Player();
         Dice dice = new Dice();
         Board board = new Board();
