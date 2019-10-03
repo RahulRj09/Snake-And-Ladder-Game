abf4d539254146115927234b46b0bc3a0cdbafc7
RahulRj09 <rahul18@navgurukul.org>
2019-10-03 10:01:13 +0530

    Add.constructor in Player class

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -1,4 +1,8 @@
 package game;
 
 public class Player {
+    Token token;
+    public Player(Token token) {
+        this.token = token;
+    }
 }
--- src/test/java/game/GameTest.java
+++ src/test/java/game/GameTest.java
@@ -2,13 +2,22 @@ package game;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class GameTest {
     @Test
     public void gameShouldBeRun() {
-        Yard yard = new Yard();
-        Player player = new Player();
+        Token red = new Token("red");
+        Token green = new Token("green");
+        List<Token> tokens = new ArrayList<>();
+        tokens.add(red);
+        tokens.add(green);
+        Yard yard = new Yard(tokens);
+        Player player = new Player(red);
+        Player player1 = new Player(green);
         Dice dice = new Dice();
         Board board = new Board();
     }
