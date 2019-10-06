e680dc3981a9e0e144bbb09184f0ccacce3a1a11
RahulRj09 <rahul18@navgurukul.org>
2019-10-07 02:05:34 +0530

    Add. class DiceDatabaseHelper

---
+++ src/main/java/DatabaseHelper/DiceDatabaseHelper.java
@@ -0,0 +1,4 @@
+package DatabaseHelper;
+
+public class DiceDatabaseHelper {
+}
--- src/main/java/gamehandler/DiceHandler.java
+++ src/main/java/gamehandler/DiceHandler.java
@@ -1,13 +1,30 @@
 package gamehandler;
 
+import DatabaseHelper.DiceDatabaseHelper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import game.Dice;
+import org.json.simple.JSONObject;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.List;
 
 public class DiceHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-
+        String emailId = exchange.getRequestURI().getQuery();
+        List<String> emailIdA = Arrays.asList(emailId.split("="));
+        System.out.println(emailIdA.get(1));
+        DiceDatabaseHelper diceDatabaseHelper = new DiceDatabaseHelper();
+        JSONObject numberOnDice = new JSONObject();
+        numberOnDice.put("dice", new Dice().roll());
+        String response = numberOnDice.toString();
+        exchange.getResponseHeaders().set("Content-Type", "appication/json");
+        exchange.sendResponseHeaders(200, response.length());
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
