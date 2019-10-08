e50bb76e9ea55c1a6fdbc5211ed9d469b06daf76
RahulRj09 <rahul18@navgurukul.org>
2019-10-09 01:32:13 +0530

    refactor. code formatting

--- src/main/java/resources/static/js/dice.js
+++ src/main/java/resources/static/js/dice.js
@@ -1,7 +1,7 @@
-   $('#dice').click(function(){
-        $.emailId = localStorage.getItem("emailId");
-        console.log($.emailId);
-            $.get("/getNumberOnDice?emailId="+$.emailId, function(data, status){
-                    $('p').append(data.position);
-                });
+$('#dice').click(function () {
+    $.emailId = localStorage.getItem("emailId");
+    console.log($.emailId);
+    $.get("/getNumberOnDice?emailId=" + $.emailId, function (data, status) {
+        $('p').append(data.position);
     });
+});
--- src/main/java/resources/static/js/profile.js
+++ src/main/java/resources/static/js/profile.js
@@ -1,16 +1,17 @@
-   $(window).on("load", GetAllProperties);
-            function GetAllProperties() {
-	            $.emailId = localStorage.getItem("emailId");
-	            $.get("/getProfile?emailId=" + $.emailId, function (data, status) {
-	                if(data){
-		                $('h2').append(data.Profile[0].name);
-		                $('#name').append(data.Profile[0].name);
-		                $('#email').append(data.Profile[0].email);
-		                $('#win').append(data.Profile[0].win)
-		                $('#loss').append(data.Profile[0].loss)
-		                $('#total').append(data.Profile[0].total)
-		            }else{
-		                 $('h2').append("User not found")
-		            }
-	            });
-            }
+$(window).on("load", GetAllProperties);
+
+function GetAllProperties() {
+    $.emailId = localStorage.getItem("emailId");
+    $.get("/getProfile?emailId=" + $.emailId, function (data, status) {
+        if (data) {
+            $('h2').append(data.Profile[0].name);
+            $('#name').append(data.Profile[0].name);
+            $('#email').append(data.Profile[0].email);
+            $('#win').append(data.Profile[0].win)
+            $('#loss').append(data.Profile[0].loss)
+            $('#total').append(data.Profile[0].total)
+        } else {
+            $('h2').append("User not found")
+        }
+    });
+}
