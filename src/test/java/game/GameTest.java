b5a93c6425d7f59bd30ecdd7db08a105c1cf8812
RahulRj09 <rahul18@navgurukul.org>
2019-10-09 16:43:53 +0530

    Fix. test case

--- src/test/java/game/GameTest.java
+++ src/test/java/game/GameTest.java
@@ -2,6 +2,8 @@ package game;
 
 import org.junit.Test;
 
+import java.io.*;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,23 +11,33 @@ import static org.junit.Assert.*;
 
 public class GameTest {
     @Test
-    public void gameShouldBeRun() {
-        RedYard red = new RedYard(new Token(), "red");
-        GreenYard green = new GreenYard(new Token(), "green");
+    public void gameShouldBeRun() throws IOException, SQLException {
+        setEmailId();
+        Yard green = new Yard(new Token(), "green");
+        Yard red = new Yard(new Token(), "red");
         List<Yard> yards = new ArrayList<>();
         yards.add(red);
         yards.add(green);
-        Player player = new Player(red);
-        Player player1 = new Player(green);
+        Player rahul = new Player(red, "hello@gmail.com");
+        Player nitesh = new Player(green, "computer@gmail.com");
         List<Player> players = new ArrayList<>();
-        players.add(player);
-        players.add(player1);
+        players.add(rahul);
+        players.add(nitesh);
         Dice dice = new Dice();
         Board board = new Board(yards, dice);
         Game game = new Game(board, players);
         while (game.isRunning()) {
             game.play();
             game.isRunning();
+        }
+    }
+    public void setEmailId() throws IOException {
+        String emailId ="hello@gmail.com";
+        File file = new File("/Users/rahul.joshi/SnakeAndLadderGame/src/main/java/resources/static/email.txt");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+            bw.write(emailId);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
