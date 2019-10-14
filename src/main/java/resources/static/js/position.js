53ac53c0948b06180eb9880dd5efb5820b1614a1
RahulRj09 <rahul18@navgurukul.org>
2019-10-14 22:58:42 +0530

    Add. image tom.png

--- src/main/java/resources/static/js/position.js
+++ src/main/java/resources/static/js/position.js
@@ -1,3 +1,56 @@
+let positions = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    21: "twentyOne",
+    22: "twentyTwo",
+    23: "twentyTree",
+    24: "twentyFour",
+    25: "twentyFive",
+    26: "twentySix",
+    27: "twentySeven",
+    28: "twentyEight",
+    29: "twentyNine",
+    30: "thirty",
+    31: "thirtyOne",
+    32: "thirtyTwo",
+    33: "thirtyThree",
+    34: "thirtyFour",
+    35: "thirtyFive",
+    36: "thirtySix",
+    37: "thirtySeven",
+    38: "thirtyEight",
+    39: "thirtyNine",
+    40: "fourty",
+    41: "fourtyOne",
+    42: "fourtyTwo",
+    43: "fourtyTree",
+    44: "fourtyFour",
+    45: "fourtyFive",
+    46: "fourtySix",
+    47: "fourtySeven",
+    48: "fourtyEight",
+    49: "fourtyNine",
+    50: "fifty"
+}
+
 $('#dice').click(function () {
     $('#position').empty();
     $('#currentPositionEmailId').empty();
@@ -7,7 +60,7 @@ $('#dice').click(function () {
             let position = data.details[0].position;
             let emailId = data.details[0].emailId;
             if (emailId === $.emailId) {
-                $('#' + position).prepend('<img id="theImg" src="../static/images/snakes-and-ladders-board-game.jpg" />')
+                $('#'+positions[position]).prepend('<img id="theImg" src="../static/images/snakes-and-ladders-board-game"  alt="you"/>')
             }
             $('#currentPositionEmailId').append(emailId);
             $('#position').append(position);
@@ -17,8 +70,6 @@ $('#dice').click(function () {
                     $('#winner').append("Winner " + data);
                 });
             }
-        } else {
-            $('#position').append(0);
         }
     });
 });
