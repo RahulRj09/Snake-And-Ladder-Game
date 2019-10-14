fb088bd99ac2fa5d35dfa85c761bd0acc40af084
RahulRj09 <rahul18@navgurukul.org>
2019-10-14 21:12:10 +0530

    Add. DisplayImage.js file

--- src/main/java/resources/static/js/position.js
+++ src/main/java/resources/static/js/position.js
@@ -4,12 +4,17 @@ $('#dice').click(function () {
     $.emailId = localStorage.getItem("emailId");
     $.get("/getCurrentPosition?emailId=" + $.emailId, function (data) {
         if (data) {
-            $('#currentPositionEmailId').append(data.details[0].emailId);
-            $('#position').append(data.details[0].position);
-            if (data.details[0].position === 100) {
+            let position = data.details[0].position;
+            let emailId = data.details[0].emailId;
+            if (emailId === $.emailId) {
+                $('#' + position).prepend('<img id="theImg" src="../static/images/snakes-and-ladders-board-game.jpg" />')
+            }
+            $('#currentPositionEmailId').append(emailId);
+            $('#position').append(position);
+            if (position === 100) {
                 $.get("/getWinner", function (data) {
                     console.log(data)
-                    $('#winner').append("Winner "+ data);
+                    $('#winner').append("Winner " + data);
                 });
             }
         } else {
