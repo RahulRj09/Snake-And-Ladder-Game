55a668272fcb9ee5b89dec37eec5c1f3664aec14
RahulRj09 <rahul18@navgurukul.org>
2019-10-14 12:45:02 +0530

    refactor. return type

--- src/main/java/DatabaseHelper/WinnerDatabaseHelper.java
+++ src/main/java/DatabaseHelper/WinnerDatabaseHelper.java
@@ -1,7 +1,25 @@
 package DatabaseHelper;
 
-public class WinnerDatabaseHelper {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-    public void getWinner() {
+public class WinnerDatabaseHelper {
+    private DatabaseConnection databaseConnection = new DatabaseConnection();
+
+    public String getWinner() {
+        String emailId = "";
+        String selectQuery = String.format("SELECT * FROM gameCurrentState WHERE position = '%d'", 100);
+        try (Connection conn = databaseConnection.connect();
+             PreparedStatement preparedStatement = conn.prepareStatement(selectQuery)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                emailId += resultSet.getString("emailId");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return emailId;
     }
 }
