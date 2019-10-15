295b8c60c6bf7a1c6d369ae3208a3ea8c4482d57
RahulRj09 <rahul18@navgurukul.org>
2019-10-15 15:30:21 +0530

    convert Uri to list for emailId

--- src/main/java/authentication/Logout.java
+++ src/main/java/authentication/Logout.java
@@ -4,9 +4,14 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Logout implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        String emailId = exchange.getRequestURI().getQuery();
+        List<String> emailIdA = Arrays.asList(emailId.split("="));
+
     }
 }
