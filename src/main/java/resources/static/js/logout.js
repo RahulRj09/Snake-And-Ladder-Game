d21e4accb759a3bcb1fb5f6619239fe900f0b86a
RahulRj09 <rahul18@navgurukul.org>
2019-10-15 16:27:14 +0530

    Add. modalBox in home.html

--- src/main/java/resources/static/js/logout.js
+++ src/main/java/resources/static/js/logout.js
@@ -1,10 +1,11 @@
+const demo = document.getElementById("logoutModal");
 $('#logout').click(function () {
     $.emailId = localStorage.getItem("emailId");
     $.get("/logout?emailId=" + $.emailId, function (data) {
         if (data === "false") {
             window.location.replace("/SnakeAndLadderGame/login.html");
-        } else if(data==="true"){
-            console.log("hello");
+        } else if (data === "true") {
+            demo.style.display = "block";
         }
 
     });
