19116dfbd16ffc8b1c803c0b2a81455f6af2c8e9
RahulRj09 <rahul18@navgurukul.org>
2019-10-15 15:55:32 +0530

    refactor

--- src/main/java/authentication/Logout.java
+++ src/main/java/authentication/Logout.java
@@ -1,9 +1,11 @@
 package authentication;
 
+import DatabaseHelper.TokenDatabaseHelper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,6 +14,14 @@ public class Logout implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
         String emailId = exchange.getRequestURI().getQuery();
         List<String> emailIdA = Arrays.asList(emailId.split("="));
-
+        TokenDatabaseHelper tokenDatabaseHelper = new TokenDatabaseHelper();
+        boolean result = tokenDatabaseHelper.positionRowExistsOrNotForCurrentUser(emailIdA.get(1));
+        if (!result) {
+            String response = String.valueOf(false);
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
--- src/main/java/resources/static/js/logout.js
+++ src/main/java/resources/static/js/logout.js
@@ -1,6 +1,8 @@
 $('#logout').click(function () {
     $.emailId = localStorage.getItem("emailId");
     $.get("/logout?emailId=" + $.emailId, function (data) {
-
+        if (data === "false") {
+            window.location.replace("/SnakeAndLadderGame/login.html");
+        }
     });
 });
