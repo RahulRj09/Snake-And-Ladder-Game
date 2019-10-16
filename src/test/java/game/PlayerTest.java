4be08910d9889ac4f5a25768a5f1c4045f5978be
RahulRj09 <rahul18@navgurukul.org>
2019-10-16 11:29:45 +0530

    fix. test cases after remove color field

--- src/test/java/game/PlayerTest.java
+++ src/test/java/game/PlayerTest.java
@@ -17,6 +17,7 @@ import static org.mockito.Mockito.when;
 public class PlayerTest {
     @Test
     public void tokenShouldBeMoveOut() throws SQLException {
+
         Yard red = new Yard(new Token());
         Player rahul = new Player(red, "hello1@gmail.com");
         Dice dice = spy(new Dice());
