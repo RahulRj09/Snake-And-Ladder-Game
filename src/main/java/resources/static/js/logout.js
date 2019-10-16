83d52e1ead5ec17ec8b8091912994fccc7384791
RahulRj09 <rahul18@navgurukul.org>
2019-10-16 14:26:52 +0530

    refactor. Url use /SnakeAndLadderGame/login instead of /SnakeAndLadderGame/login.html

--- src/main/java/resources/static/js/logout.js
+++ src/main/java/resources/static/js/logout.js
@@ -3,14 +3,14 @@ $.emailId = localStorage.getItem("emailId");
 $('#logout').click(function () {
     $.get("/logout?emailId=" + $.emailId, function (data) {
         if (data === "false") {
-            window.location.replace("/SnakeAndLadderGame/login.html");
+            window.location.replace("/SnakeAndLadderGame/login");
         } else if (data === "true") {
             modalForLogout.style.display = "block";
         }
     });
 });
 $('#save').click(function () {
-    window.location.replace("/SnakeAndLadderGame/login.html");
+    window.location.replace("/SnakeAndLadderGame/login");
 })
 
 $('#exit').click(function () {
