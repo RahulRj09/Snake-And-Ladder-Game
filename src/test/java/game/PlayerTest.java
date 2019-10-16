02873bc8956182228ddbb2342a9edfb80d96f82c
RahulRj09 <rahul18@navgurukul.org>
2019-10-16 09:42:55 +0530

    tokenShouldBeMove test case pass

--- src/test/java/game/PlayerTest.java
+++ src/test/java/game/PlayerTest.java
@@ -30,8 +30,23 @@ public class PlayerTest {
         WinnerDatabaseHelper winnerDatabaseHelper = new WinnerDatabaseHelper();
         winnerDatabaseHelper.tableTruncate();
     }
+
     @Test
-    public void tokenShouldBeMove(){
+    public void tokenShouldBeMove() throws FileNotFoundException, SQLException {
+        Yard red = new Yard(new Token(), "red");
+        Player rahul = new Player(red, "hello1@gmail.com");
+        Dice dice = spy(new Dice());
+        when(dice.roll()).thenReturn(1);
+        rahul.play(dice);
+        when(dice.roll()).thenReturn(6);
+        rahul.play(dice);
+        TokenDatabaseHelper tokenDatabaseHelper = new TokenDatabaseHelper();
+        JSONObject position = tokenDatabaseHelper.getCurrentPosition("hello1@gmail.com");
+        JSONArray positionArray = (JSONArray) position.get("details");
+        JSONObject positionA = (JSONObject) positionArray.get(0);
+        assertEquals(7, positionA.get("position"));
+        WinnerDatabaseHelper winnerDatabaseHelper = new WinnerDatabaseHelper();
+        winnerDatabaseHelper.tableTruncate();
 
     }
 }
