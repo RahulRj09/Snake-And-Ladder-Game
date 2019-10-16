64111847cd5783e7cb29be84cba802714fbb5a15
RahulRj09 <rahul18@navgurukul.org>
2019-10-16 13:31:30 +0530

    Add. login.js file

---
+++ src/main/java/resources/static/js/login.js
@@ -0,0 +1,12 @@
+$(function () {
+    $("#my-form").submit(function (e) {
+        let emailId = $("#email").val();
+        let password = $("#password").val();
+        localStorage.setItem("emailId", emailId);
+        $.get("/login?emailId=" + emailId + "password=" + password, function (data) {
+            if (data === true) {
+                window.location.replace("/SnakeAndLadderGame/home.html");
+            }
+        });
+    });
+});
