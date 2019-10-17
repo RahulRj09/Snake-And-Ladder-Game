c37697552cb54d3f7057a724dda19d9ede469a25
RahulRj09 <rahul18@navgurukul.org>
2019-10-17 17:10:59 +0530

    Add. test case pass placeMethodShouldBeWorking

--- src/test/java/game/TokenTest.java
+++ src/test/java/game/TokenTest.java
@@ -2,9 +2,13 @@ package game;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class TokenTest {
     @Test
     public void placeMethodShouldBeWorking(){
-
+        Token token = new Token();
+        token.place("rahul@gmail.com",1);
+        assertEquals(1,token.getPosition("rahul@gmail.com"));
     }
 }
