7c11c4f8a3bc5285b92bc91afe053a7a62b1c67d
RahulRj09 <rahul18@navgurukul.org>
2019-10-17 17:15:15 +0530

    fix test case

--- src/test/java/game/TokenTest.java
+++ src/test/java/game/TokenTest.java
@@ -1,14 +1,18 @@
 package game;
 
+import DatabaseHelper.Winner;
 import org.junit.Test;
+
+import java.sql.SQLException;
 
 import static org.junit.Assert.assertEquals;
 
 public class TokenTest {
     @Test
-    public void placeMethodShouldBeWorking(){
+    public void placeMethodShouldBeWorking() throws SQLException {
         Token token = new Token();
         token.place("rahul@gmail.com",1);
         assertEquals(1,token.getPosition("rahul@gmail.com"));
+        new Winner().tableTruncate();
     }
 }
