6f80e537d713dcde6451384c014456b4a8ff92c1
RahulRj09 <rahul18@navgurukul.org>
2019-10-17 16:58:34 +0530

    test case pass diceShouldBeRoll with numberOnDice 1

--- src/test/java/game/DiceTest.java
+++ src/test/java/game/DiceTest.java
@@ -3,11 +3,15 @@ package game;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 public class DiceTest {
 
     @Test
     public void diceShouldBeRoll() {
-
+        Dice dice = spy(new Dice());
+        when(dice.roll()).thenReturn(1);
+        assertEquals(1,dice.roll());
     }
 }
