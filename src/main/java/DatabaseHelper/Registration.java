c85dd0047e1e2b99afe1b18dab2427abc58bcc15
RahulRj09 <rahul18@navgurukul.org>
2019-10-22 11:47:51 +0530

    initial commit

--- src/main/java/DatabaseHelper/Registration.java
+++ src/main/java/DatabaseHelper/Registration.java
@@ -2,28 +2,42 @@ package DatabaseHelper;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Registration {
     private DatabaseConnection databaseConnection = new DatabaseConnection();
 
     public void insert(String name, String emailId, String password) {
-        usersAlreadyExistsOrNot(emailId);
-        String sql = "insert into users(name,emailId,password) VALUES(?,?,?)";
-        try (Connection conn = databaseConnection.connect();
-             PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, emailId);
-            preparedStatement.setString(3, password);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+        boolean result = usersAlreadyExistsOrNot(emailId);
+        if (result) {
+            String sql = "insert into users(name,emailId,password) VALUES(?,?,?)";
+            try (Connection conn = databaseConnection.connect();
+                 PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, emailId);
+                preparedStatement.setString(3, password);
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
         }
+        System.out.println("done");
     }
 
 
     private boolean usersAlreadyExistsOrNot(String emailId) {
-        return true;
+        String selectQuery = String.format("SELECT * FROM users WHERE emailId = '%s'", emailId);
+        try (Connection conn = databaseConnection.connect();
+             PreparedStatement preparedStatement = conn.prepareStatement(selectQuery)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
-
 }
